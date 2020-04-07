afa56f59a414a5fd9f5f9365def7f4e1a79f7161
CuestaCodes <cuestacodes@gmail.com>
2020-04-08 09:51:31 +1000

    Added first class and instance variables and class constructor

--- com/cuestacodes/codeacademy/TransitCalculator.java
+++ com/cuestacodes/codeacademy/TransitCalculator.java
@@ -1,7 +1,33 @@
 package com.cuestacodes.codeacademy;
 
 class TransitCalculator {
+  static double singleRide = 2.75;
+
+  static double sevenDayUnlimited = 33.00;
+
+  static double thirtyDayUnlimited = 127.00;
+
+  int numberOfDays;
+
+  int numberOfRides;
+
+  public TransitCalculator(int numOfDays, int numOfRides){
+    numberOfDays = numOfDays;
+    numberOfRides = numOfRides;
+  }
+
+  public double unlimited7Price(){
+    int numOfSevenPasses = numberOfDays / 7;
+
+    if (numberOfDays % 7 != 0){
+      numOfSevenPasses++;
+    }
+
+    return numOfSevenPasses * sevenDayUnlimited;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Welcome to cuestacodes' TransitCalculator");
+    int daysTravelling;
+    int individualRides;
   }
 }
