79feba5097cc19866839ba99e0b93729af10e293
CuestaCodes <cuestacodes@gmail.com>
2020-04-07 21:14:03 +1000

    Adding first variable with an undo of previous commit

--- com/cuestacodes/codeacademy/TransitCalculator.java
+++ com/cuestacodes/codeacademy/TransitCalculator.java
@@ -3,5 +3,7 @@ package com.cuestacodes.codeacademy;
 class TransitCalculator {
   public static void main(String[] args) {
     System.out.println("Welcome to cuestacodes' TransitCalculator");
+
+    int i = 1;
   }
 }
