14e74f0ca56b21d43aac3a3dd9ad5805ef1ad42a
CuestaCodes <cuestacodes@gmail.com>
2020-04-07 21:25:55 +1000

    Refreshing class back to start after tests with git

--- com/cuestacodes/codeacademy/TransitCalculator.java
+++ com/cuestacodes/codeacademy/TransitCalculator.java
@@ -3,8 +3,5 @@ package com.cuestacodes.codeacademy;
 class TransitCalculator {
   public static void main(String[] args) {
     System.out.println("Welcome to cuestacodes' TransitCalculator");
-
-    int i = 1;
-    i = 3;
   }
 }
