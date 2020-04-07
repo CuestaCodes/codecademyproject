71c6fe284a708e46befa8014fb1f4e6d759c8da0
CuestaCodes <cuestacodes@gmail.com>
2020-04-07 21:03:19 +1000

    first commit

---
+++ com/cuestacodes/codeacademy/TransitCalculator.java
@@ -0,0 +1,7 @@
+package com.cuestacodes.codeacademy;
+
+class TransitCalculator {
+  public static void main(String[] args) {
+    System.out.println("Welcome to cuestacodes' TransitCalculator");
+  }
+}
