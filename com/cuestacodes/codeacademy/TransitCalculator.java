414bfca44689d7e2d3fe9fdbe28cfe96aef1baa3
CuestaCodes <cuestacodes@gmail.com>
2020-04-08 21:40:24 +1000

    Added concession fares

--- com/cuestacodes/codeacademy/TransitCalculator.java
+++ com/cuestacodes/codeacademy/TransitCalculator.java
@@ -5,32 +5,46 @@ import java.util.ArrayList;
 class TransitCalculator {
   double[] fareOptions = {2.75, 33.00, 127.00};
 
+  double[] concFareOptions = {1.35, 16.50, 63.50};
+
   String[] fareTypes = {"Pay-per-ride", "7-Day Unlimited", "30-Day Unlimited"};
+
+  double[] ridePrices = new double [3];
 
   int numberOfDays;
 
   int numberOfRides;
 
-  double[] ridePrices = new double [3];
+  boolean concession = false;
 
-  public TransitCalculator(int numOfDays, int numOfRides){
-    if (numOfDays < 0 || numOfDays > 30 || numOfRides < 0) {
+  public TransitCalculator(int numOfDays, int numOfRides, boolean hasDisability, int age){
+    if (numOfDays < 0 || numOfDays > 30 || numOfRides < 0 || age < 0) {
       throw new IllegalArgumentException();
-   } else {
-     numberOfDays = numOfDays;
-     numberOfRides = numOfRides;
-   }
+    } else {
+      numberOfDays = numOfDays;
+      numberOfRides = numOfRides;
+
+      if (hasDisability || age > 65){
+        concession = true;
+      }
+    }
   }
 
   // calculates overall price per ride using 7-Day Unlimited option
   public double unlimited7Price(){
     int numOfSevenPasses = numberOfDays / 7;
 
+    double priceTotal;
+
     if (numberOfDays % 7 != 0){
       numOfSevenPasses++;
     }
 
-    double priceTotal = numOfSevenPasses * fareOptions[1];
+    if (concession) {
+      priceTotal = numOfSevenPasses * concFareOptions[1];
+    } else {
+      priceTotal = numOfSevenPasses * fareOptions[1];
+    }
 
     double sevenPrice = priceTotal / numberOfRides;
 
@@ -39,9 +53,15 @@ class TransitCalculator {
 
   // calculates the price per ride for each fare option
   public double[] getRidePrices(){
-    ridePrices[0] = fareOptions[0];
+    if (concession) {
+      ridePrices[0] = concFareOptions[0];
+      ridePrices[2] = concFareOptions[2] / numberOfRides;
+    } else {
+      ridePrices[0] = fareOptions[0];
+      ridePrices[2] = fareOptions[2] / numberOfRides;
+    }
+
     ridePrices[1] = unlimited7Price();
-    ridePrices[2] = fareOptions[2] / numberOfRides;
 
     return ridePrices;
   }
@@ -54,6 +74,8 @@ class TransitCalculator {
 
     double pricePerRide;
 
+    String concessionAvail = "";
+
     getRidePrices();
 
     for (int i = 0 ; i < ridePrices.length ; i++){
@@ -64,7 +86,11 @@ class TransitCalculator {
       }
     }
 
-    String bestFareFind =  "You should get the " + bestFareType + " at $" + bestFare + " per ride";
+    if (concession) {
+      concessionAvail = "You qualify for a concession fare. ";
+    }
+
+    String bestFareFind = concessionAvail + "You should get the " + bestFareType + " at $" + bestFare + " per ride";
 
     return bestFareFind;
   }
@@ -73,14 +99,13 @@ class TransitCalculator {
     int daysTravelling;
     int individualRides;
 
-    // testing commuters with varying days travelled and number of rides during that time
     ArrayList<TransitCalculator> commuters = new ArrayList<TransitCalculator>();
 
-    commuters.add(new TransitCalculator(22,50));
-    commuters.add(new TransitCalculator(26,54));
-    commuters.add(new TransitCalculator(5,12));
-    commuters.add(new TransitCalculator(14,28));
-    commuters.add(new TransitCalculator(7,2));
+    commuters.add(new TransitCalculator(22, 50, false, 67));
+    commuters.add(new TransitCalculator(26, 54, false, 25));
+    commuters.add(new TransitCalculator(5, 12, true, 35));
+    commuters.add(new TransitCalculator(14, 28, false, 29));
+    commuters.add(new TransitCalculator(7, 2, false, 65));
 
     for (int i = 0 ; i < commuters.size() ; i++){
       System.out.println(commuters.get(i).getBestFare());
